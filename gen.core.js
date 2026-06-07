83568631d32626e12e09eff9129de819bf3e4c7a
iki1uc <entertrainer@web.de>
2026-06-08 01:17:41 +0200

    Update gen.core.js

--- gen.core.js
+++ gen.core.js
@@ -1,1 +1,25 @@
 
+// /GEN/gen.core.js
+
+export const GEN = [
+  {
+    tree: "Science",
+    fruits: ["Scan", "Measure", "Axis"],
+    life: "analysis"
+  },
+  {
+    tree: "SEEÜ",
+    fruits: ["Flow", "Module", "Beam"],
+    life: "engine"
+  },
+  {
+    tree: "8ALL",
+    fruits: ["Report", "Pulse", "Strang"],
+    life: "network"
+  },
+  {
+    tree: "iki1uc",
+    fruits: ["Root", "Djinn", "X4"],
+    life: "core"
+  }
+];
