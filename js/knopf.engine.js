0928c8a53e480f5baa670d0a1297083f8ebe1adb
iki1uc <entertrainer@web.de>
2026-06-08 01:38:56 +0200

    Update knopf.engine.js

--- js/knopf.engine.js
+++ js/knopf.engine.js
@@ -1,1 +1,27 @@
+// /js/knopf.engine.js
+
+import { tune } from './axes.core.js';
+import { scanParam } from './scan.core.js';
+
+export function triggerKnopf(name, sharp = 1.0) {
+
+  // 1) Begriff scharf/unscharf skalieren
+  const tuning = tune(name, sharp);
+
+  // 2) Begriff im Logikraum scannen
+  const scan = scanParam(name);
+
+  // 3) Ergebnis erzeugen
+  return {
+    knopf: name,
+    sharp,
+    scan,
+    axes: tuning.axes,
+    result: {
+      impact: scan.grade * sharp,
+      mode: scan.ableitung,
+      axisCount: tuning.axes.length
+    }
+  };
+}
 
